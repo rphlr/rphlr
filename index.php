bb1b085f7c9843d2115ef7624cb75ed3badb5de0
Raphael <rrouille@student.42lausanne.ch>
2023-12-30 10:12:33 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Raphael's Lunar Profile</title>
+</head>
+
+<body>
+
+	<h1>Hello from Lunar Surfaces, I'm Raphael! 🌖👨‍💻✨</h1>
+
+	<p align="center">
+		<img src="https://komarev.com/ghpvc/?username=rphlr&label=Lunar+Landings&color=green" alt="Lunar Landings" />
+	</p>
+
+	<p align="center">
+		<img src="https://raw.githubusercontent.com/rphlr/rphlr/main/imgs/Duckstronaut_Coding_Odyssey.gif"
+			alt="Duck on the Moon Coding" />
+	</p>
+
+	<p>Quacking in code from the moon! As the galaxy's first duck developer, I'm debugging the stars one quack at a
+		time. 🚀🦆💻 Whether you're here to explore celestial code or share a space-bean of knowledge, you're in for an
+		astronomical treat! 🌠✨</p>
+
+	<p>Currently, I am undergoing training at 42 School in Lausanne. 📚👨‍💻🚀</p>
+
+	<p align="center">
+		<a href="https://github.com/oakoudad/badge42">
+			<img src="https://badge.mediaplus.ma/darkblue/Rrouille?1337Badge=off&UM6P=off" alt="rrouille's 42 stats" />
+		</a>
+	</p>
+
+	<hr />
+
+	<h3>🌌 Stellar Repositories 🚀📚</h3>
+
+	<p><strong>Featured Projects:</strong></p>
+	<ul>
+		<li>
+			<strong><a href="https://github.com/rphlr/42-Subjects">42-Subjects</a>:</strong> Your launchpad to the 42
+			School curriculum with project PDFs in English and French. 📚🌐🚀
+		</li>
+		<li>
+			<strong><a href="https://github.com/rphlr/42-Evals">42-Evals</a>:</strong> A telescope to peer into the
+			evaluation process with official 42 School evaluation sheets. 🔭✅📄
+		</li>
+	</ul>
+
+	<p>Check out the live version of our 42-Evals repository to navigate the stars of peer evaluation with ease: 🌟👨‍🚀
+		<a href="https://rphlr.github.io/42-Evals">42-Evals Web Page</a></p>
+
+	<p><strong>Explore More Repositories:</strong></p>
+
+	<details>
+		<summary>📚 42 School projects</summary>
+		<!-- Content for 42 School projects -->
+		<!-- Similar to the Markdown, the detailed content for each section can be added here -->
+	</details>
+
+	<details>
+		<summary>🔧 Tools and Utilities</summary>
+		<!-- Content for Tools and Utilities -->
+	</details>
+
+	<details>
+		<summary>👾 Fun and Miscellaneous</summary>
+		<!-- Content for Fun and Miscellaneous -->
+	</details>
+
+	<hr />
+
+	<h2>🦆 About the Quack Developer 🦆</h2>
+	<picture>
+		<img align="right" src="imgs/coding.gif" width="250px" alt="Coding Duck" />
+	</picture>
+
+	<ul>
+		<li>🖥️ Former industrial draftsman, now orchestrating constellations of code.</li>
+		<li>🌃 Passionate about astronomy and astrophysics.</li>
+		<li>🚒 Volunteering as a firefighter and involved in various directorial roles.</li>
+		<li>🛰️ On a mission to collaborate on projects that are out of this world!</li>
+	</ul>
+
+	<h2>🚀 Skills and Interests</h2>
+
+	<ul>
+		<li>🌌 Mastery in SCCM, server setups, and multiple programming languages.</li>
+		<li>🛠️ Seasoned in web design, photo and video editing.</li>
+		<li>🪐 Always learning new tech from HTML to Python, React to Node.js.</li>
+		<li>🌠 Tools of the trade: WSL, Git, VSCode, VIM, and creative suites.</li>
+	</ul>
+
+	<hr />
+
+	<h3>📊 Coding Constellations</h3>
+
+	<div align="center">
+		<img src="https://github-readme-stats.vercel.app/api?username=rphlr&show_icons=true&locale=en&layout=compact&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40"
+			alt="Raphael's Github stats">
+		<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rphlr&layout=compact&theme=midnight-purple"
+			alt="Top Languages" />
+	</div>
+
+	<hr />
+
+	<h3>📡 Get in Touch</h3>
+
+	<div align="center">
+		<a href="mailto:rrouille@student.42lausanne.ch">
+			<img src="https://raw.githubusercontent.com/rphlr/rphlr/main/imgs/email.png" width="30" height="30"
+				alt="Email" />
+		</a>
+		<a href="https://SuisseLibre.t.me">
+			<img src="https://raw.githubusercontent.com/rphlr/rphlr/main/imgs/telegram.png" width="30" height="30"
+				alt="Telegram" />
+		</a>
+		<a href="https://www.linkedin.com/in/rphlr">
+			<img src="https://raw.githubusercontent.com/rphlr/rphlr/main/imgs/linkedin.png" width="30" height="30"
+				alt="LinkedIn" />
+		</a>
+	</div>
+
+	<hr />
+
+	<p>🦆 Repository made with ❤️ by <a href="https://github.com/rphlr">Raphael</a>. Quack on and keep coding!</p>
+
+	<img src="https://hit.yhype.me/github/profile?user_id=81936759" alt="GitHub Profile Hit Count" />
+
+</body>
+
+</html>
